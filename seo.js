89c830ad8804881bee4b415a2bbe56cf27db69e7
ThisisDanielaG <146487775+ThisisDanielaG@users.noreply.github.com>
2024-01-24 15:58:07 -0800

    Create seo.js

---
+++ seo.js
@@ -0,0 +1,11 @@
+         <!-- SEO -->
+<script type="application/ld+json">
+    {
+      "@context" : "https://www.diversefires.com",
+      "@type" : "WebSite",
+      "name" : "Diverse Fires",
+      "url" : "https://diversefires.com/"
+ "description": "Daniela Gonzalez’s personal website to showcase professional and artistic pursuits.",
+
+    }
+  </script>

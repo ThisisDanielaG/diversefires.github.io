da40dc002004493976429ae2e08ca915944aef8c
ThisisDanielaG <146487775+ThisisDanielaG@users.noreply.github.com>
2024-01-24 16:07:28 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
-$(document).ready(function(){
-  $("a").on('click', function(event) {
-    if (this.hash !== "") {
-      event.preventDefault();
-      var hash = this.hash;
-      $('body,html').animate({
-      scrollTop: $(hash).offset().top
-      }, 1200, function(){
-      window.location.hash = hash;
-     });
-     } 
-    });
-});
-
 var width = $(window).width(); 
-
 window.onscroll = function(){
-if ((width >= 900)){
-    if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        $("#middle").css("background-size","150% auto");
-    }else{
-        $("#middle").css("background-size","100% auto");        
-    }
 }
-};
+
+function magnify(imglink){
+    $("#img_here").css("background",`url('${imglink}') center center`);
+    $("#magnify").css("display","flex");
+    $("#magnify").addClass("animated fadeIn");
+    setTimeout(function(){
+        $("#magnify").removeClass("animated fadeIn");
+    },800);
+}
+
+function closemagnify(){
+    $("#magnify").addClass("animated fadeOut");
+    setTimeout(function(){
+        $("#magnify").css("display","none");
+        $("#magnify").removeClass("animated fadeOut");
+        $("#img_here").css("background",`url('') center center`);
+    },800);
+}
 
 setTimeout(function(){
     $("#loading").addClass("animated fadeOut");
@@ -30,4 +26,19 @@ setTimeout(function(){
       $("#loading").removeClass("animated fadeOut");
       $("#loading").css("display","none");
     },800);
-},1450);
+},1650);
+
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('body,html').animate({
+        scrollTop: $(hash).offset().top
+        }, 1800, function(){
+        window.location.hash = hash;
+       });
+       } 
+      });
+  });
+  

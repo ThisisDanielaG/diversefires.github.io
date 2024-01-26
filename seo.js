0abe55650b9786c0e30cdcbae5f8170d4f15f7c5
ThisisDanielaG <146487775+ThisisDanielaG@users.noreply.github.com>
2024-01-25 22:56:14 -0800

    Update seo.js

--- seo.js
+++ seo.js
@@ -1,11 +1,24 @@
-         <!-- SEO -->
-<script type="application/ld+json">
-    {
-      "@context" : "https://www.diversefires.com",
-      "@type" : "WebSite",
-      "name" : "Diverse Fires",
-      "url" : "https://diversefires.com/"
- "description": "Daniela Gonzalez’s personal website to showcase professional and artistic pursuits.",
+ 
 
-    }
-  </script>
+<script>
+       const form = document.querySelector("#form")
+       const submitButton = document.querySelector("#submit")
+       const scriptURL = 'https://script.google.com/macros/s/AKfycbxLNpaWlW0_lNxVmmvn0uAN2_GsCLmGQATDbSzuBlRQlw8CtI6zPn210yxVtBciObURjw/exec'
+
+       form.addEventListener('submit', e => {
+         submitButton.disabled = true
+         e.preventDefault()
+         let requestBody = new FormData(form)
+         fetch(scriptURL, { method: 'POST', body: requestBody})
+           .then(response => {
+              alert('Success!', response)
+              submitButton.disabled = false
+             })
+           .catch(error => {
+           alert('Error!', error.message)
+             submitButton.disabled = false
+
+           }
+           )
+       })
+    </script>

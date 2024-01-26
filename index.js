865ae48179bb9deb161df3a795fdbbbc3b74a476
ThisisDanielaG <146487775+ThisisDanielaG@users.noreply.github.com>
2024-01-26 13:01:51 -0800

    removed
    const prev = document.getElementById('prev-btn')
    const next = document.getElementById('next-btn')
    const list = document.getElementById('item-list')
    const itemWidth = 150
    const padding = 10
    
    prev.addEventListener('click',()=>{
      list.scrollLeft -= (itemWidth + padding)
    })
    next.addEventListener('click',()=>{
      list.scrollLeft += (itemWidth + padding)
    })

--- index.js
+++ index.js
@@ -32,16 +32,3 @@ setTimeout(function(){
     },800);
 },1450);
 
-
-const prev = document.getElementById('prev-btn')
-const next = document.getElementById('next-btn')
-const list = document.getElementById('item-list')
-const itemWidth = 150
-const padding = 10
-
-prev.addEventListener('click',()=>{
-  list.scrollLeft -= (itemWidth + padding)
-})
-next.addEventListener('click',()=>{
-  list.scrollLeft += (itemWidth + padding)
-})

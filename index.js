0f6dedc869d3b6183ea538effe0aeada6cba4691
ThisisDanielaG <146487775+ThisisDanielaG@users.noreply.github.com>
2024-01-26 12:55:20 -0800

    added container js
    const prev = document.getElementById('prev-btn')
    const next = document.getElementById('next-btn')
    const list = document.getElementById('item-list')
    const itemWidth = 150
    const padding = 10
    
    prev.addEventListener('click',()=>{
      list.scrollLeft -= (itemWidth + padding)
    })
    next.addEventListener('click',()=>{
      list.scrollLeft += (itemWidth + padding)
    })

--- index.js
+++ index.js
@@ -31,3 +31,17 @@ setTimeout(function(){
       $("#loading").css("display","none");
     },800);
 },1450);
+
+
+const prev = document.getElementById('prev-btn')
+const next = document.getElementById('next-btn')
+const list = document.getElementById('item-list')
+const itemWidth = 150
+const padding = 10
+
+prev.addEventListener('click',()=>{
+  list.scrollLeft -= (itemWidth + padding)
+})
+next.addEventListener('click',()=>{
+  list.scrollLeft += (itemWidth + padding)
+})
